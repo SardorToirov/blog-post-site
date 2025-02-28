ffd296464f3febd5cbc486fe0ad0cba727d3f2f2
Sardor Toirov <toirovsardor62@gmail.com>
2025-02-28 14:36:50 +0500

    complated project

--- config/settings.py
+++ config/settings.py
@@ -59,12 +59,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'first_db',
+        'USER': 'posgre',
+        'PASSWORD': '1213',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
